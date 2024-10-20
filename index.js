18ce1643b8681e1790fcf5bca71743934878f787
Michael Telakowicz <mat210@uakron.edu>
2024-10-20 14:47:36 -0400

    Fix .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const port = 5000; // You can choose any port
