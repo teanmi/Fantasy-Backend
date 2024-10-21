dfbe1c2f7a0b42aa267a58555ff3c7604ede60cd
Ethan Reusser <ecr38@uakron.edu>
2024-10-21 18:41:27 -0400

    Add viewAllPlayers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
+
+// Controllers
 const leagueController = require("../controllers/leagueController");
 const teamController = require("../controllers/teamController");
+const playerController = require("../controllers/playerController");
 
-// leagues routes
+// Leagues Routes
 router.post("/leagues/create", leagueController.createLeague);
 router.post("/leagues/idview", leagueController.viewLeagueByID);
 router.post("/leagues/nameview", leagueController.viewLeagueByName);
@@ -12,6 +15,9 @@ router.post("/leagues/nameview", leagueController.viewLeagueByName);
 router.post("/teams/create", teamController.createTeam);
 router.post("/teams/idview", teamController.viewTeamByID);
 router.post("/teams/nameandleagueview", teamController.viewTeamByNameAndLeague);
+
+// Player Routes
+router.post("/players/viewall", playerController.viewAllPlayers);
 
 // Export the router
 module.exports = router;
